8f7c0f16831f07fc475aeac8ff5b6121ce3dd2e3
NwotYknuf <jlafille@ensc.fr>
2018-06-02 19:04:48 +0200

    Pacman renderer

---
+++ view/PacmanRenderer.java
@@ -0,0 +1,72 @@
+package com.view;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.model.Pacman;
+
+public class PacmanRenderer {
+
+	
+	private SpriteBatch spriteBatch;
+	
+	private int pacmanAnimationState = 0;
+	private long elapsedTimeSinceLastUpdate = 0;
+	private final int pacmanAnimationFrames = 2;
+	private final long frameDuration = 200; // in milliseconds
+	private final int pacmanTextureHeight = 16;
+	private final int pacmanTextureWidth = 16;
+	
+	public PacmanRenderer(SpriteBatch sb) {
+		this.spriteBatch = sb;
+	}
+	
+	public void drawPacman(Pacman pac, float ppuX, float ppuY, int mazeHeight, long elapsedTime) {
+			
+		updatePacman(elapsedTime);
+		
+		int frame = 0;
+		
+		switch(pac.getDirection()) {
+		case Down :
+			frame = 1 * pacmanAnimationFrames;
+			break;
+		case Left :
+			frame = 2 * pacmanAnimationFrames;
+			break;
+		case Right :
+			frame = 3 * pacmanAnimationFrames;
+		case Up :
+			frame = 4 * pacmanAnimationFrames;	
+			break;
+		default : 
+			break;
+		}
+		
+		TextureRegion text = new TextureRegion(
+			TextureFactory.getInstance().getTexture(pac.getClass()),
+			(frame + pacmanAnimationState) * pacmanTextureWidth,
+			0,
+			pacmanTextureWidth,
+			pacmanTextureHeight);
+		
+		this.spriteBatch.draw(
+				text,
+				pac.getY() * ppuX,
+				(mazeHeight - pac.getX() - 1) * ppuY,
+				pac.getWidth() * ppuX,
+				pac.getHeight() * ppuY
+		);
+	}	
+
+	private void updatePacman(long elapsedTime) {
+		elapsedTimeSinceLastUpdate += elapsedTime;
+		
+		if(elapsedTimeSinceLastUpdate > frameDuration) {
+			pacmanAnimationState ++;
+			elapsedTimeSinceLastUpdate = 0;
+			if(pacmanAnimationState >= pacmanAnimationFrames) {
+				pacmanAnimationState = 0;
+			}
+		}		
+	}
+}
--- view/WorldRenderer.java
+++ view/WorldRenderer.java
@@ -14,15 +14,8 @@ public class WorldRenderer{
 	private float ppuY;
 	
 	private MazeRenderer mazeRenderer;
-	private MovingElementRenderer pacmanRenderer;
-	
-	private int pacmanAnimationState = 0;
-	private long elapsedTimeSinceLastUpdate = 0;
-	private final int pacmanAnimationFrames = 2;
-	private final long frameDuration = 200; // in milliseconds
-	private final int pacmanTextureHeight = 16;
-	private final int pacmanTextureWidth = 16;
-	
+	private PacmanRenderer pacmanRenderer;
+		
 	public WorldRenderer(World world, SpriteBatch sb){
 
 		this.world = world;
@@ -31,67 +24,18 @@ public class WorldRenderer{
 		ppuY = 16f;
 		
 		this.mazeRenderer = new MazeRenderer(sb);
-		
+		this.pacmanRenderer = new PacmanRenderer(sb);
 	}
 	
 	public void render(long elapsedTime){
 		this.spriteBatch.begin();
 		
 		mazeRenderer.drawMaze(world.getMaze(), ppuX, ppuY);
-		
-		updatePacman(elapsedTime);
-		drawPacaman();
-		
+		pacmanRenderer.drawPacman(world.getPacman(), ppuX, ppuY, world.getHeight(), elapsedTime);
 		this.spriteBatch.end();
 	}
 
-	private void updatePacman(long elapsedTime) {
-		elapsedTimeSinceLastUpdate += elapsedTime;
-		
-		if(elapsedTimeSinceLastUpdate > frameDuration) {
-			pacmanAnimationState ++;
-			elapsedTimeSinceLastUpdate = 0;
-			if(pacmanAnimationState >= pacmanAnimationFrames) {
-				pacmanAnimationState = 0;
-			}
-		}		
-	}
 
-	private void drawPacaman() {
-		
-		Pacman pac = world.getPacman();
-				
-		int frame = 0;
-		
-		switch(pac.getDirection()) {
-		case Down :
-			frame = 1 * pacmanAnimationFrames;
-			break;
-		case Left :
-			frame = 2 * pacmanAnimationFrames;
-			break;
-		case Right :
-			frame = 3 * pacmanAnimationFrames;
-		case Up :
-			frame = 4 * pacmanAnimationFrames;				
-		}
-		
-		TextureRegion text = new TextureRegion(
-			TextureFactory.getInstance().getTexture(pac.getClass()),
-			(frame + pacmanAnimationState) * pacmanTextureWidth,
-			0,
-			pacmanTextureWidth,
-			pacmanTextureHeight
-			);
-		
-		 
-		this.spriteBatch.draw(
-				text,
-				pac.getY() * ppuX,
-				(world.getMaze().getHeight() - pac.getX() - 1) * ppuY,
-				pac.getWidth() * ppuX,
-				pac.getHeight() * ppuY
-		);
-	}
 
+	
 }
