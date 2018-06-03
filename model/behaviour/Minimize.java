a4845876552c5eabd2d032c33d144406706bc3a2
Zeyel <tim.branstett@gmail.com>
2018-06-03 18:52:24 +0200

    Approchement par minimisation

--- model/behaviour/Minimize.java
+++ model/behaviour/Minimize.java
@@ -15,6 +15,65 @@ public class Minimize extends Behaviour{
 		int diffX = this.getX() - this.getHost().getX();
 		int diffY = this.getY() - this.getHost().getY();
 		
-		return Direction.None;
+		if (this.getHost().canMove(higher(diffX, diffY)))
+			return higher(diffX, diffY);
+		else if (this.getHost().canMove(second(diffX, diffY)))
+			return second(diffX, diffY);
+		else if (this.getHost().canMove(third(diffX, diffY)))
+			return third(diffX, diffY);
+		else
+			return lower(diffX, diffY);
+			
+	}
+	
+	public Direction higher(int diffX, int diffY) {
+		if (Math.abs(diffX) < Math.abs(diffY)) {
+			if (diffY < 0)
+				return Direction.Up;		
+			else
+				return Direction.Down;
+	} else
+		if (diffX < 0)
+			return Direction.Left;
+		else
+			return Direction.Right;
+	}
+
+	public Direction second(int diffX, int diffY) {
+		if (Math.abs(diffX) < Math.abs(diffY)) {
+			if (diffX < 0)
+				return Direction.Left;		
+			else
+				return Direction.Right;
+	} else
+		if (diffX < 0)
+			return Direction.Up;
+		else
+			return Direction.Down;
+	}
+	
+	public Direction third(int diffX, int diffY) {
+		if (Math.abs(diffX) < Math.abs(diffY)) {
+			if (-diffX < 0)
+				return Direction.Left;		
+			else
+				return Direction.Right;
+	} else
+		if (-diffX < 0)
+			return Direction.Up;
+		else
+			return Direction.Down;
+	}
+	public Direction lower(int diffX, int diffY) {
+		if (Math.abs(diffX) < Math.abs(diffY)) {
+			if (-diffY < 0)
+				return Direction.Up;		
+			else
+				return Direction.Down;
+	} else
+		if (-diffX < 0)
+			return Direction.Left;
+		else
+			return Direction.Right;
 	}
 }
